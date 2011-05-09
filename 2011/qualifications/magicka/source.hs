1d3f71d6e0ec57840b23cb7ca1d590dc9e6eba5e
dsouza <dsouza@bitforest.org>
2011-05-09 18:22:28 -0300

    solution to magicka problem

---
+++ 2011/qualifications/magicka/source.hs
@@ -0,0 +1,78 @@
+module Main (main) where
+
+import Control.Monad
+import Data.Maybe
+
+import qualified Data.Map as M
+import qualified Data.Set as S
+
+type Base = Char
+
+type BaseL = [Base]
+
+type Composition = M.Map (Base,Base) Base
+
+type Opposition  = S.Set (Base,Base)
+
+pair :: Base -> Base -> (Base,Base)
+pair a b
+  | a <= b    = (a,b)
+  | otherwise = (b,a)
+
+combine :: Composition -> BaseL -> Maybe BaseL
+combine db (x:y:xs) = case (M.lookup (pair x y) db)
+                      of Nothing -> Nothing
+                         Just e  -> Just (e : xs)
+combine _ xs        = Nothing
+
+oppose :: Opposition -> BaseL -> Maybe BaseL
+oppose db (x:xs) = let shouldClear = or (map (\y -> S.member (pair x y) db) xs)
+                   in if (shouldClear)
+                      then Just []
+                      else Nothing
+oppose _ xs      = Nothing
+
+invoke :: Composition -> Opposition -> BaseL -> Base -> BaseL
+invoke cdb odb base e = fromJust (cBase `mplus` oBase `mplus` Just newBase)
+  where newBase = e : base
+        cBase   = combine cdb newBase
+        oBase   = oppose odb newBase
+
+showBaseL :: BaseL -> String
+showBaseL [] = "[]"
+showBaseL xs = '[' : myShow (reverse xs) ']'
+  where myShow (x:y:xs) suffix = x : ',' : ' ' : myShow (y:xs) suffix
+        myShow (x:xs) suffix   = x : [suffix]
+
+parseComposition :: [String] -> ([String], Composition)
+parseComposition (x:xs) = (drop n xs, cdb)
+  where n = read x
+        cdb = M.fromList $ map entry (take n xs)
+        entry [a,b,v] = (pair a b, v)
+
+parseOpposition :: [String] -> ([String], Opposition)
+parseOpposition (x:xs) = (drop n xs, odb)
+  where n = read x
+        odb = S.fromList $ map entry (take n xs)
+        entry [a,b] = pair a b
+
+parseBase :: [String] -> ([String], BaseL)
+parseBase (x:xs) = (drop 2 xs, baseL)
+  where n = read x
+        baseL = take n (head xs)
+
+main :: IO ()
+main = do { n <- fmap read getLine
+          ; replicateM n getLine >>= foldM_ (\n line -> myPrint n (myExec line) >> return (n+1)) 1
+          }
+  where myExec xs = let input = words xs
+                        (input',cdb)  = parseComposition input
+                        (input'',odb) = parseOpposition input'
+                        (_,baseL)     = parseBase input''
+                    in foldl (invoke cdb odb) [] baseL
+        
+        myPrint n baseL = do { putStr "Case #"
+                             ; putStr (show n)
+                             ; putStr ": "
+                             ; putStrLn (showBaseL baseL)
+                             }
